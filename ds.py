5c0b3cfb8e56261bb066633251e90fea46a3989c
Pavankalyany <60839440+Pavankalyany@users.noreply.github.com>
2020-08-25 15:14:09 +0530

    Create ds.py

---
+++ ds.py
@@ -0,0 +1,17 @@
+#lists
+car=["lamborgini","swift","maruthi suzuki"]
+print(car[0])
+print(car)
+print(len(car))
+
+#tuple
+mytuple=("pk","ak","gk")
+print(mytuple)
+print(len(mytuple))
+print(mytuple[0])
+
+#dictionaries
+mydict={"c":5,"a":6,"g":7}
+print("before deletion" ,mydict)
+del mydict["a"]
+print("after deletion",mydict)
